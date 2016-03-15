b492c3a1cf16b7ee71d87c700af4404a924a6c44
imrokas <rokas@imrokas.com>
2016-03-15 14:51:49 -0700

    ex.1 implemented basic express server to serve static file, and json data

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
 
 var jsonData = {count: 12, message: 'hey'};
+var PORT = 3000;
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html', function(err, data) {
+    if(err)
+      res.status(500).send(err);
+  });
+});
+
+app.get('/data', function(req, res) {
+  res.json(jsonData);
+});
+
+app.listen(PORT, function(){
+  console.log('Server is running on port ', PORT);
+});
